aaa9dbf261879698f789075cb158424fcb0ee580
AlejoKA <a.kapranov@gmail.com>
2018-12-03 17:55:07 +0300

    Initial Commit

---
+++ SimpleLoginScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleLoginScreen
+//
+//  Created by Aleksey Kapranov on 03/12/2018.
+//  Copyright © 2018 Alejo_KA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
