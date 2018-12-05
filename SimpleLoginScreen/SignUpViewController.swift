5c464346e557a4cc069c24d0b8bab5e09be045f9
AlejoKA <a.kapranov@gmail.com>
2018-12-05 23:08:04 +0300

    Create class SignUpViewController.

---
+++ SimpleLoginScreen/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  SimpleLoginScreen
+//
+//  Created by Aleksey Kapranov on 05/12/2018.
+//  Copyright © 2018 Alejo_KA. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
