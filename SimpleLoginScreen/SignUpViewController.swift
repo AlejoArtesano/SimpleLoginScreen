ee1ffe2f4cf259aad6560bbd20c8c39bef65b22e
AlejoKA <a.kapranov@gmail.com>
2018-12-06 00:01:04 +0300

    add prepare method to class SignUpViewController.

--- SimpleLoginScreen/SignUpViewController.swift
+++ SimpleLoginScreen/SignUpViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    
+    @IBOutlet weak var signupNameTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != nil {
+            let destinationVC = segue.destination as! WelcomeViewController
+            destinationVC.text = (signupNameTextField?.text)!
+        }
+    
+    }
 }
