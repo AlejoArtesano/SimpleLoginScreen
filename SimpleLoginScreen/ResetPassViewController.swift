d4afe148fff82c40a0aa5a877a6c0b445cdde887
AlejoKA <a.kapranov@gmail.com>
2018-12-05 23:12:07 +0300

    Create class ResetPassViewController

---
+++ SimpleLoginScreen/ResetPassViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResetPassViewController.swift
+//  SimpleLoginScreen
+//
+//  Created by Aleksey Kapranov on 05/12/2018.
+//  Copyright © 2018 Alejo_KA. All rights reserved.
+//
+
+import UIKit
+
+class ResetPassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
